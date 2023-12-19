a93c76cc5567fdf792e9053f335716116595e354
ThinhBeo <59316091+Thinhle2701@users.noreply.github.com>
2023-12-19 18:53:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,29 @@ mongoose.connect(
   }
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+//   next();
+// });
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://selfcheckout.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
   next();
 });
 

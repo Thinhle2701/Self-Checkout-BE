c3ce554385bebf16e13e48b5a6453db6d91cc5b4
ThinhBeo <59316091+Thinhle2701@users.noreply.github.com>
2024-01-01 12:45:54 +0700

    Update sse.js

--- controllers/sse.js
+++ controllers/sse.js
@@ -11,152 +11,153 @@ const donation = {
 };
 
 router.post("/transfer_rfid_tag", async (req, res) => {
-  const cartID = req.body.data.cartID;
-  try {
-    const checkoutCartObject = await checkoutCart.findOne({ cartID: cartID });
-    if (checkoutCartObject) {
-      const uuid = req.body.data.RFID.split("||")[0];
-      const productID = req.body.data.RFID.split("||")[1];
-      try {
-        const rfidTag = await RFID.findOne({
-          uuid: uuid,
-          productID: productID,
-        });
+  // const cartID = req.body.data.cartID;
+  // try {
+  //   const checkoutCartObject = await checkoutCart.findOne({ cartID: cartID });
+  //   if (checkoutCartObject) {
+  //     const uuid = req.body.data.RFID.split("||")[0];
+  //     const productID = req.body.data.RFID.split("||")[1];
+  //     try {
+  //       const rfidTag = await RFID.findOne({
+  //         uuid: uuid,
+  //         productID: productID,
+  //       });
 
-        if (rfidTag) {
-          try {
-            const Pro = await product.findOne({ id: productID });
-            if (Pro) {
-              if (checkoutCartObject.RFID.length === 0) {
-                console.log("insert");
-                var uuidArr = [];
-                await uuidArr.push(rfidTag.uuid);
-                const cartItem = [
-                  {
-                    id: "item_" + Number(checkoutCartObject.RFID.length + 1),
-                    itemnumber: Number(checkoutCartObject.RFID.length + 1),
-                    name: Pro.name,
-                    productID: productID,
-                    image: Pro.image,
-                    quantity: 1,
-                    price: Pro.price,
-                    uuid: uuidArr,
-                  },
-                ];
+  //       if (rfidTag) {
+  //         try {
+  //           const Pro = await product.findOne({ id: productID });
+  //           if (Pro) {
+  //             if (checkoutCartObject.RFID.length === 0) {
+  //               console.log("insert");
+  //               var uuidArr = [];
+  //               await uuidArr.push(rfidTag.uuid);
+  //               const cartItem = [
+  //                 {
+  //                   id: "item_" + Number(checkoutCartObject.RFID.length + 1),
+  //                   itemnumber: Number(checkoutCartObject.RFID.length + 1),
+  //                   name: Pro.name,
+  //                   productID: productID,
+  //                   image: Pro.image,
+  //                   quantity: 1,
+  //                   price: Pro.price,
+  //                   uuid: uuidArr,
+  //                 },
+  //               ];
 
-                const totalPrice = Pro.price;
-                const RFIDArr = [];
-                RFIDArr.push(rfidTag.uuid);
+  //               const totalPrice = Pro.price;
+  //               const RFIDArr = [];
+  //               RFIDArr.push(rfidTag.uuid);
 
-                await checkoutCart.updateOne(
-                  { cartID: cartID },
-                  {
-                    $set: {
-                      cartItem: cartItem,
-                      totalPrice: totalPrice,
-                      RFID: RFIDArr,
-                      scanned: true,
-                    },
-                  }
-                );
-                console.log("insert successfully");
-              } else {
-                var found = checkoutCartObject.RFID.includes(uuid);
-                console.log(found);
-                if (found === true) {
-                  console.log("exist item");
-                } else {
-                  var checkExistItem = false;
-                  for (let i = 0; i < checkoutCartObject.cartItem.length; i++) {
-                    if (
-                      checkoutCartObject.cartItem[i].productID === productID
-                    ) {
-                      checkExistItem = true;
-                      console.log("add quantity");
+  //               await checkoutCart.updateOne(
+  //                 { cartID: cartID },
+  //                 {
+  //                   $set: {
+  //                     cartItem: cartItem,
+  //                     totalPrice: totalPrice,
+  //                     RFID: RFIDArr,
+  //                     scanned: true,
+  //                   },
+  //                 }
+  //               );
+  //               console.log("insert successfully");
+  //             } else {
+  //               var found = checkoutCartObject.RFID.includes(uuid);
+  //               console.log(found);
+  //               if (found === true) {
+  //                 console.log("exist item");
+  //               } else {
+  //                 var checkExistItem = false;
+  //                 for (let i = 0; i < checkoutCartObject.cartItem.length; i++) {
+  //                   if (
+  //                     checkoutCartObject.cartItem[i].productID === productID
+  //                   ) {
+  //                     checkExistItem = true;
+  //                     console.log("add quantity");
 
-                      var insertQTYobj = checkoutCartObject;
-                      var currUUIDArr = insertQTYobj.cartItem[i].uuid;
-                      insertQTYobj.cartItem[i].quantity =
-                        Number(insertQTYobj.cartItem[i].quantity) + 1;
-                      insertQTYobj.cartItem[i].price =
-                        Number(insertQTYobj.cartItem[i].price) +
-                        Number(Pro.price);
-                      currUUIDArr.push(rfidTag.uuid);
-                      insertQTYobj.totalPrice =
-                        Number(insertQTYobj.totalPrice) + Number(Pro.price);
-                      insertQTYobj.cartItem[i].uuid = currUUIDArr;
-                      var RFIDArr = [];
-                      RFIDArr.push(rfidTag.uuid);
-                      await checkoutCart.updateOne(
-                        { cartID: cartID },
-                        {
-                          $set: {
-                            cartItem: insertQTYobj.cartItem,
-                            totalPrice: insertQTYobj.totalPrice,
-                            RFID: RFIDArr,
-                          },
-                        }
-                      );
-                    }
-                  }
-                  if (checkExistItem === false) {
-                    console.log("add new item");
-                    var insertNewObj = checkoutCartObject;
-                    var itemArr = insertNewObj.cartItem;
-                    var uuidArr = [];
-                    await uuidArr.push(rfidTag.uuid);
+  //                     var insertQTYobj = checkoutCartObject;
+  //                     var currUUIDArr = insertQTYobj.cartItem[i].uuid;
+  //                     insertQTYobj.cartItem[i].quantity =
+  //                       Number(insertQTYobj.cartItem[i].quantity) + 1;
+  //                     insertQTYobj.cartItem[i].price =
+  //                       Number(insertQTYobj.cartItem[i].price) +
+  //                       Number(Pro.price);
+  //                     currUUIDArr.push(rfidTag.uuid);
+  //                     insertQTYobj.totalPrice =
+  //                       Number(insertQTYobj.totalPrice) + Number(Pro.price);
+  //                     insertQTYobj.cartItem[i].uuid = currUUIDArr;
+  //                     var RFIDArr = [];
+  //                     RFIDArr.push(rfidTag.uuid);
+  //                     await checkoutCart.updateOne(
+  //                       { cartID: cartID },
+  //                       {
+  //                         $set: {
+  //                           cartItem: insertQTYobj.cartItem,
+  //                           totalPrice: insertQTYobj.totalPrice,
+  //                           RFID: RFIDArr,
+  //                         },
+  //                       }
+  //                     );
+  //                   }
+  //                 }
+  //                 if (checkExistItem === false) {
+  //                   console.log("add new item");
+  //                   var insertNewObj = checkoutCartObject;
+  //                   var itemArr = insertNewObj.cartItem;
+  //                   var uuidArr = [];
+  //                   await uuidArr.push(rfidTag.uuid);
 
-                    var newElementItem = {
-                      id:
-                        "item_" +
-                        Number(checkoutCartObject.cartItem.length + 1),
-                      itemnumber: Number(
-                        checkoutCartObject.cartItem.length + 1
-                      ),
-                      name: Pro.name,
-                      productID: productID,
-                      image: Pro.image,
-                      quantity: 1,
-                      price: Pro.price,
-                      uuid: uuidArr,
-                    };
-                    itemArr.push(newElementItem);
-                    insertNewObj.totalPrice =
-                      Number(insertNewObj.totalPrice) + Number(Pro.price);
-                    var RFIDArr = insertNewObj.RFID;
-                    RFIDArr.push(rfidTag.uuid);
-                    await checkoutCart.updateOne(
-                      { cartID: cartID },
-                      {
-                        $set: {
-                          cartItem: itemArr,
-                          totalPrice: insertNewObj.totalPrice,
-                          RFID: RFIDArr,
-                        },
-                      }
-                    );
-                  }
-                }
-              }
-            } else {
-              throw new Error("not exist product");
-            }
-          } catch (err) {
-            console.log(err.message);
-          }
-        } else {
-          throw new Error("not exist RFID");
-        }
-      } catch (err) {
-        console.log(err.message);
-      }
-    } else {
-      throw new Error("not exist cart");
-    }
-  } catch (err) {
-    console.log(err.message);
-  }
-  //console.log(req.body);
+  //                   var newElementItem = {
+  //                     id:
+  //                       "item_" +
+  //                       Number(checkoutCartObject.cartItem.length + 1),
+  //                     itemnumber: Number(
+  //                       checkoutCartObject.cartItem.length + 1
+  //                     ),
+  //                     name: Pro.name,
+  //                     productID: productID,
+  //                     image: Pro.image,
+  //                     quantity: 1,
+  //                     price: Pro.price,
+  //                     uuid: uuidArr,
+  //                   };
+  //                   itemArr.push(newElementItem);
+  //                   insertNewObj.totalPrice =
+  //                     Number(insertNewObj.totalPrice) + Number(Pro.price);
+  //                   var RFIDArr = insertNewObj.RFID;
+  //                   RFIDArr.push(rfidTag.uuid);
+  //                   await checkoutCart.updateOne(
+  //                     { cartID: cartID },
+  //                     {
+  //                       $set: {
+  //                         cartItem: itemArr,
+  //                         totalPrice: insertNewObj.totalPrice,
+  //                         RFID: RFIDArr,
+  //                       },
+  //                     }
+  //                   );
+  //                 }
+  //               }
+  //             }
+  //           } else {
+  //             throw new Error("not exist product");
+  //           }
+  //         } catch (err) {
+  //           console.log(err.message);
+  //         }
+  //       } else {
+  //         throw new Error("not exist RFID");
+  //       }
+  //     } catch (err) {
+  //       console.log(err.message);
+  //     }
+  //   } else {
+  //     throw new Error("not exist cart");
+  //   }
+  // } catch (err) {
+  //   console.log(err.message);
+  // }
+
+  console.log(req.body);
 
   return res.json({ message: "Success" });
 });
